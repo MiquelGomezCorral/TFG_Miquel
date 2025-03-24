c59526119274599fb0606804852b2c7cf8f7d1e9
Miquel Gómez <miquelgc2003@gmail.com>
2025-03-24 19:45:56 +0100

    updated setup

--- TFG/setup.sh
+++ TFG/setup.sh
@@ -18,4 +18,8 @@ pip install "transformers==4.38.2" \
 git config --global user.email "miquelgc2003@gmail.com"
 git config --global user.name "Miquel Gómez Corral"
 
+
+git clone git@bitbucket.org:solverrepos/module_ocr_llm.git
+git clone https://github.com/clovaai/donut.git
+
 echo "✅ Environment setup complete!"
