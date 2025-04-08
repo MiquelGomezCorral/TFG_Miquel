d5de4d07f1c7c75a4d7d4985294bf4b42e61e0a1
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-04-08 22:01:00 +0200

    Extracted field from pretrained ocr

--- app/TFG/scripts_ocr_llm/test_orc_llm.py
+++ app/TFG/scripts_ocr_llm/test_orc_llm.py
@@ -98,9 +98,9 @@ def main(args):
         # f"ocr_finetuned_{i*5}x5_v1" for i in range(1,5+1)
         # "ocr_finetuned_5x5_v1",
         "ocr_finetuned_4x5_v1",
-        "ocr_finetuned_3x5_v1",
-        "ocr_finetuned_5x2_v1",
-        "ocr_finetuned_5x1_v1",
+        # "ocr_finetuned_3x5_v1",
+        # "ocr_finetuned_5x2_v1",
+        # "ocr_finetuned_5x1_v1",
     ]
     
     # ================ Process files ================
@@ -137,12 +137,9 @@ def main(args):
                 
                 # Send document to ORC to extract content
                 print(" - Extracting content with OCR...", end="\r")
-                document_content, pages = document_to_orc(ocr_client, file_io, prebuilt_model=model)
+                document_content, pages, fields_content, json_output  = document_to_orc(ocr_client, file_io, prebuilt_model=model)
                 MODEL_TIME_TRACKER.track(tag="Extracting content.", space=False)
                 
-                print(f"{document_content = }")
-                print(f"{pages = }")
-
                 if args.llm:
                     # Define the prompt and send it to send to the LLM
                     print(" - Creating structured output with LLM...", end="\r")
@@ -193,11 +190,15 @@ def prepare_document(file_io: io.BytesIO, document_path: str) -> io.BytesIO:
 
 def document_to_orc(ocr_client: AzureDocumentIntelligenceClient, file_io: io.BytesIO, prebuilt_model: str = None):
     # IMPORTANT TO CHANGE THE TYPE OF DOCUMENT THAT THE OCR HAS TO READ.
-    pages: list[str] = ocr_client.read_document(file_io, prebuilt_model=prebuilt_model)
+    pages, result = ocr_client.read_document(file_io, prebuilt_model=prebuilt_model)
 
+    # check_orc_outpu(result)
+    fields_content, json_output = get_fields_from_resutl(result)
+    print(f"{fields_content = }")
+    
     document_content: str = "\n\n ------- PAGE BREAK ------- \n\n".join(pages)
 
-    return document_content, pages
+    return document_content, pages, fields_content, json_output 
 
 
 def document_to_llm(llm_client: AzureOpenAILanguageModel, document_content: str):
@@ -249,6 +250,106 @@ def save_output(save_path, ground_truths, predictions):
     # print(json_output)
     return json_output
 
+def get_fields_from_resutl(result) -> tuple[dict, str]:
+    """Convers the Result object return from a azure document intelligence OCR into a dict and a json format with just the field
+    Example of results fields
+    (
+        'Buyer', 
+        {
+            'type': 'string',
+            'valueString': 'Andre Quinn', 
+            'content': 'Andre Quinn', 
+            'boundingRegions': [
+                {
+                    'pageNumber': 1,
+                    'polygon': [
+                        2.718,
+                        3.2488,
+                        3.6761,
+                        3.2532,
+                        3.6754,
+                        3.4154,
+                        2.7173,
+                        3.411
+                    ]
+                }
+            ],
+            'confidence': 0.906,
+            'spans': [
+                {
+                    'offset': 128,
+                    'length': 11
+                }
+            ]
+        }
+    )
+    Args:
+        result (_type_): Result object from a azure document intelligence OCR
+
+    Returns:
+        tuple[dict, str]: Structured output fields in dict and in json format
+    """
+    documents_fields = [document.fields.items() for document in result.documents]
+    fields_content = []
+    for document_fields in documents_fields:
+        fields_dict = dict()
+        for fields in document_fields:
+            # fields_dict[fields[0]] = fields[1]["content"]
+            field_name = f"value{fields[1]['type'].capitalize()}" # make the first letter of the type capillar so it ends like 'valueString' for example
+            fields_dict[fields[0]] = fields[1][field_name]
+            
+            # print(f" - Fields: {fields}")
+        fields_content.append(fields_dict)
+        
+    json_output = json.dumps(fields_content, indent=4)
+    
+    return fields_content, json_output
+
+
+def check_orc_outpu(result):
+    # poller = document_intelligence_client.begin_analyze_document(
+    #     model_id, AnalyzeDocumentRequest(url_source=formUrl)
+    # )
+    
+    for idx, document in enumerate(result.documents):
+        print("--------Analyzing document #{}--------".format(idx + 1))
+        print("Document has type {}".format(document.doc_type))
+        print("Document has confidence {}".format(document.confidence))
+        print("Document was analyzed by model with ID {}".format(result.model_id))
+        for name, field in document.fields.items():
+            print("......found field of type '{}' with value '{}' and with confidence {}".format(field.type, field.content, field.confidence))
+
+
+    # iterate over tables, lines, and selection marks on each page
+    for page in result.pages:
+        print("\nLines found on page {}".format(page.page_number))
+        for line in page.lines:
+            print("...Line '{}'".format(line.content.encode('utf-8')))
+        for word in page.words:
+            print(
+                "...Word '{}' has a confidence of {}".format(
+                    word.content.encode('utf-8'), word.confidence
+                )
+            )
+        if page.selection_marks:
+            for selection_mark in page.selection_marks:
+                print(
+                    "...Selection mark is '{}' and has a confidence of {}".format(
+                        selection_mark.state, selection_mark.confidence
+                    )
+                )
+
+    for i, table in enumerate(result.tables):
+        print("\nTable {} can be found on page:".format(i + 1))
+        for region in table.bounding_regions:
+            print("...{}".format(i + 1, region.page_number))
+        for cell in table.cells:
+            print(
+                "...Cell[{}][{}] has content '{}'".format(
+                    cell.row_index, cell.column_index, cell.content.encode('utf-8')
+                )
+            )
+    print("-----------------------------------")
 
 if __name__ == "__main__":
 
@@ -257,19 +358,19 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-d", "--dataset_path", type=str, default="./datasets_finetune/outputs/FATURA/test",
+        "-d", "--dataset_path", type=str, default="./final_dataset_fatura/test",
         help="Local path from ./app to the dataset."
     )
     parser.add_argument(
-        "s", "--save_path", type=str, default=".TFG/outputs/ocr_llm",
+        "-s", "--save_path", type=str, default=".TFG/outputs/ocr_llm",
         help="Local path from ./app to the folder where all the outputs will be placed."
     )
     parser.add_argument(
-        "f", "--max_files", type=int, default=None,
+        "-f", "--max_files", type=int, default=None,
         help="Max files loaded from the dataset"
     )
     parser.add_argument(
-        "l", "--llm", type=bool, default=True, action="store_false",
+        "-l", "--llm", default=True, action="store_false",
         help="If to use an LLM to structure the output of the OCR. Default to 'True'"
     )
     parser.add_argument(
