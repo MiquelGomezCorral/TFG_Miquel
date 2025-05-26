26732849a233a1999007649d0a33574591546f84
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-05-26 09:26:49 +0200

    Finished merge conflict from meaningful files

--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
@@ -10,17 +9,4 @@ setup(
     install_requires=[
         # Add your dependencies from requirements.txt here
     ],
-=======
-from setuptools import setup, find_packages
-
-setup(
-    name="app",
-    version="0.1.0",
-    packages=find_packages(),
-    # packages=find_packages(where="src"),
-    # package_dir={"": "src"},
-    install_requires=[
-        # Add your dependencies from requirements.txt here
-    ],
->>>>>>> db2f1eff (changed root folder by default for imports)
 )
--- setup/compare_folders.sh
+++ setup/compare_folders.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # #!/bin/bash
 
 # # Usage: ./compare_folders.sh /path/to/folder1 /path/to/folder2
@@ -37,43 +36,3 @@ find "$FOLDER2" -type f | while read -r file2; do
         echo "ONLY IN $FOLDER2/: $rel_path"
     fi
 done
-=======
-# #!/bin/bash
-
-# # Usage: ./compare_folders.sh /path/to/folder1 /path/to/folder2
-
-FOLDER1="$1"
-FOLDER2="$2"
-
-echo "Comparing contents of:"
-echo " - $FOLDER1"
-echo " - $FOLDER2"
-echo ""
-
-# Loop through all files in FOLDER1
-find "$FOLDER1" -type f | while read -r file1; do
-    rel_path="${file1#$FOLDER1/}"
-    file2="$FOLDER2/$rel_path"
-
-    if [ -f "$file2" ]; then
-        if ! cmp -s "$file1" "$file2"; then
-            echo "MODIFIED: $rel_path"
-            echo "Differences:"
-            diff -u "$file1" "$file2"
-            echo ""
-        fi
-    else
-        echo "ONLY IN $FOLDER1/: $rel_path"
-    fi
-done
-
-# Check for files in FOLDER2 that aren't in FOLDER1
-find "$FOLDER2" -type f | while read -r file2; do
-    rel_path="${file2#$FOLDER2/}"
-    file1="$FOLDER1/$rel_path"
-
-    if [ ! -f "$file1" ]; then
-        echo "ONLY IN $FOLDER2/: $rel_path"
-    fi
-done
->>>>>>> db2f1eff (changed root folder by default for imports)
